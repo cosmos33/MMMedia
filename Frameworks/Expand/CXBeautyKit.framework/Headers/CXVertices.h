50d706bef894f5f90faf1839485d2618c2dd6388
sunfei <sun.fei@immomo.com>
2019-09-06 15:05:19 +0800

    [update] cxbeautykit to 1.9.932-tt

--- Frameworks/Expand/CXBeautyKit.framework/Headers/CXVertices.h
+++ Frameworks/Expand/CXBeautyKit.framework/Headers/CXVertices.h
@@ -13,11 +13,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CXVertices : NSObject <MTIGeometry>
 
+@property (nonatomic, readonly) NSUInteger vertexCount;
+
+@property (nonatomic, readonly) MTLPrimitiveType primitiveType;
+
 - (instancetype)init NS_UNAVAILABLE;
 
 + (instancetype)new NS_UNAVAILABLE;
 
-- (instancetype)initWithVertices:(const CXVertex * _Nonnull)vertices count:(NSInteger)count primitiveType:(MTLPrimitiveType)primitiveType NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithVertices:(const CXVertex * _Nonnull)vertices
+                           count:(NSInteger)count
+                   primitiveType:(MTLPrimitiveType)primitiveType NS_DESIGNATED_INITIALIZER;
 
 + (instancetype)squareVerticesForRect:(CGRect)rect;
 
